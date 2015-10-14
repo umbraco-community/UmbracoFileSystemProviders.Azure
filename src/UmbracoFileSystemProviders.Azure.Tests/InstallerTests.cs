9b9b44c38433031cc4288af5877b0d1f140e3d70
Jeavon Leopold <jeavon@crumpled-dog.com>
2015-10-14 22:50:36 +0100

    WIP - The beginnings of a API controller to get and set values into the xdt files

---
+++ src/UmbracoFileSystemProviders.Azure.Tests/InstallerTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Our.Umbraco.FileSystemProviders.Azure.Umbraco.Installer;
+
+namespace Our.Umbraco.FileSystemProviders.Azure.Tests
+{
+    [TestFixture]
+    public class InstallerTests
+    {
+        [Test]
+        public void CheckFirstParameterKey()
+        {
+            var parameters = InstallerController.GetParametersFromXdt();
+            Assert.AreEqual("containerName", parameters.First().Key);
+        }
+
+        [Test]
+        public void CheckNumberOfParamters()
+        {
+            var parameters = InstallerController.GetParametersFromXdt();
+            Assert.AreEqual(4, parameters.Count);
+        }
+
+    }
+}
---
+++ src/UmbracoFileSystemProviders.Azure/Umbraco/Installer/InstallerController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Hosting;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.XPath;
+using ClientDependency.Core;
+using umbraco;
+using Umbraco.Web.Mvc;
+using Umbraco.Web.WebApi;
+
+[assembly: InternalsVisibleTo("Our.Umbraco.FileSystemProviders.Azure.Tests")]
+namespace Our.Umbraco.FileSystemProviders.Azure.Umbraco.Installer
+{
+    [PluginController("FileSystemProviders")]
+    public class InstallerController : UmbracoAuthorizedApiController
+    {
+        public Dictionary<string, string> GetParameters()
+        {
+            return GetParametersFromXdt();
+        }
+
+        internal static Dictionary<string, string> GetParametersFromXdt()
+        {
+            var settings = new Dictionary<string, string>();
+
+            var filename = ("FileSystemProviders.config.install.xdt");
+
+            var document = xmlHelper.OpenAsXmlDocument(filename);
+
+            var parameters = document.SelectNodes("//Provider[@type = 'Our.Umbraco.FileSystemProviders.Azure.AzureBlobFileSystem, Our.Umbraco.FileSystemProviders.Azure']/Parameters/add");
+
+            foreach (XmlElement parameter in parameters)
+            {
+                settings.Add(parameter.GetAttribute("key"), parameter.GetAttribute("value"));
+            }
+
+            return settings;
+        }
+    }
+}
