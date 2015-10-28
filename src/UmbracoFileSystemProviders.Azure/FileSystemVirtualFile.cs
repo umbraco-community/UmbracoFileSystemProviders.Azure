42cd9e21e38a451a5009f4840f9b6f7b3252c187
Jeavon <jeavon@crumpled-dog.com>
2015-10-28 10:13:30 +0000

    Adding a null check on HttpContext.Current to allow unit tests to execute - I think really we need a HttpContext Mock (http://stackoverflow.com/questions/9624242/setting-httpcontext-current-session-in-a-unit-test)

--- src/UmbracoFileSystemProviders.Azure/FileSystemVirtualFile.cs
+++ src/UmbracoFileSystemProviders.Azure/FileSystemVirtualFile.cs
@@ -67,16 +67,19 @@ namespace Our.Umbraco.FileSystemProviders.Azure
         public override Stream Open()
         {
             // Set the response headers here. It's a bit hacky.
-            HttpCachePolicy cache = HttpContext.Current.Response.Cache;
-            cache.SetCacheability(HttpCacheability.Public);
-            cache.VaryByHeaders["Accept-Encoding"] = true;
+            if (HttpContext.Current != null)
+            {
+                HttpCachePolicy cache = HttpContext.Current.Response.Cache;
+                cache.SetCacheability(HttpCacheability.Public);
+                cache.VaryByHeaders["Accept-Encoding"] = true;
 
-            IFileSystem azureBlobFileSystem = FileSystemProviderManager.Current.GetUnderlyingFileSystemProvider("media");
-            int maxDays = ((AzureBlobFileSystem)azureBlobFileSystem).FileSystem.MaxDays;
+                IFileSystem azureBlobFileSystem = FileSystemProviderManager.Current.GetUnderlyingFileSystemProvider("media");
+                int maxDays = ((AzureBlobFileSystem)azureBlobFileSystem).FileSystem.MaxDays;
 
-            cache.SetExpires(DateTime.Now.ToUniversalTime().AddDays(maxDays));
-            cache.SetMaxAge(new TimeSpan(maxDays, 0, 0, 0));
-            cache.SetRevalidation(HttpCacheRevalidation.AllCaches);
+                cache.SetExpires(DateTime.Now.ToUniversalTime().AddDays(maxDays));
+                cache.SetMaxAge(new TimeSpan(maxDays, 0, 0, 0));
+                cache.SetRevalidation(HttpCacheRevalidation.AllCaches);
+            }
 
             return this.stream();
         }
