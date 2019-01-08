60847275ff8ba32586117ab8fa22ce252e9336b4
Warren Buckley <warren@umbraco.com>
2019-01-08 15:20:42 +0000

    More detailed error message for Boot Fail YSOD to indicate which AppSetting keys are missing

--- src/UmbracoFileSystemProviders.Azure/AzureFileSystemComposer.cs
+++ src/UmbracoFileSystemProviders.Azure/AzureFileSystemComposer.cs
@@ -21,22 +21,22 @@
 
             //Check we have all values set - otherwise make sure Umbraco does NOT boot so it can be configured correctly
             if (string.IsNullOrEmpty(containerName))
-                throw new ArgumentNullOrEmptyException("containerName");
+                throw new ArgumentNullOrEmptyException("containerName", $"The Azure File System is missing the value '{Constants.Configuration.ContainerNameKey}' from AppSettings");
 
             if (string.IsNullOrEmpty(rootUrl))
-                throw new ArgumentNullOrEmptyException("rootUrl");
+                throw new ArgumentNullOrEmptyException("rootUrl", $"The Azure File System is missing the value '{Constants.Configuration.RootUrlKey}' from AppSettings");
 
             if (string.IsNullOrEmpty(connectionString))
-                throw new ArgumentNullOrEmptyException("connectionString");
+                throw new ArgumentNullOrEmptyException("connectionString", $"The Azure File System is missing the value '{Constants.Configuration.ConnectionStringKey}' from AppSettings");
 
             if (string.IsNullOrEmpty(maxDays))
-                throw new ArgumentNullOrEmptyException("maxDays");
+                throw new ArgumentNullOrEmptyException("maxDays", $"The Azure File System is missing the value '{Constants.Configuration.MaxDaysKey}' from AppSettings");
 
             if (string.IsNullOrEmpty(useDefaultRoute))
-                throw new ArgumentNullOrEmptyException("useDefaultRoute");
+                throw new ArgumentNullOrEmptyException("useDefaultRoute", $"The Azure File System is missing the value '{Constants.Configuration.UseDefaultRouteKey}' from AppSettings");
 
             if (string.IsNullOrEmpty(usePrivateContainer))
-                throw new ArgumentNullOrEmptyException("usePrivateContainer");
+                throw new ArgumentNullOrEmptyException("usePrivateContainer", $"The Azure File System is missing the value '{Constants.Configuration.UsePrivateContainer}' from AppSettings");
 
 
             composition.RegisterFileSystem<IMediaFileSystem, MediaFileSystem>(_ => new AzureBlobFileSystem(containerName, rootUrl, connectionString, maxDays, useDefaultRoute, usePrivateContainer));
